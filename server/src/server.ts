354622db8dce00d03d1c560145bc69cee00e038f
Salomao S <souza95salomao@gmail.com>
2022-09-12 04:21:41 +0000

    update: code style and formatting

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,12 @@ import express, { Request, Response } from 'express'
 const PORT = 3333
 const app = express()
 
-app.get('/adds', (_req:Request, res:Response) => {
-	const adds = [
-		{id:1, name:'lol'}
-	]
+app.get('/adds', (_req: Request, res: Response) => {
+  const adds = [{ id: 1, name: 'lol' }]
 
-	return res.json(adds)
+  return res.json(adds)
 })
 
-app.listen(	PORT,	() => {
-	console.log(`Server is running at port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`)
 })
