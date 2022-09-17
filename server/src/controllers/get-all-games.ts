2b2399acdcf3c3a46a6588b30c457d00947840dd
Salomao S <souza95salomao@gmail.com>
2022-09-17 21:09:07 +0000

    update: finished database relationships

--- server/src/controllers/get-all-games.ts
+++ server/src/controllers/get-all-games.ts
@@ -1,11 +1,20 @@
 import { Game } from '../database/models/game'
+import { Ad } from '../database/models/ad'
 
 export class GAGController {
 	async execute () {
 		try {
-			const games = await Game.findAll({
-				attributes: ['id','title','bannerUrl']
+			const response = await Game.findAll({
+				attributes: ['id','title','bannerUrl'],
+				include: [Ad]
 			})
+
+			const games = response.map((game) => ({
+				id: game.id,
+				title: game.title,
+				bannerUrl: game.bannerUrl,
+				adsCount: game['ads'].length
+			}))
 
 			return games
 		} catch(err) {
